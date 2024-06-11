178aafc0daf878a1bd404bc095848a0538cb1ea9
Seemanshu Shukla <seemanshu.shukla11@gmail.com>
2024-06-11 07:17:24 +0530

    adding constants related to s3 bucket

---
+++ sensor/constant/s3_bucket.py
@@ -0,0 +1,4 @@
+## Defining constants for s3 bucket where we will be pushing our trained model and artifacts
+
+TRAINING_BUCKET_NAME = "scania-sensor-pipeline"
+PREDICTION_BUCKET_NAME = "sensor-datasource"
