31a32cfa8143c7abc74a5e7ff2f2b29c464ff279
Seemanshu Shukla <seemanshu.shukla11@gmail.com>
2024-06-11 07:16:20 +0530

    adding FAST API related constants

---
+++ sensor/constant/application.py
@@ -0,0 +1,5 @@
+#We will be creating a FAST API front end app for inferncing(real time prediction). This file will store
+#constants related to our frontend app
+
+APP_HOST = "0.0.0.0"  #refers to local system ip where app will be running
+APP_PORT = 8080     #refers to port for the app running on local IP
