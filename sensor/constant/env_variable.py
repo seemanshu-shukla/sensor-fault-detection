a94a8ee30901e84e0cfb99a85204fb9e5592ae10
Seemanshu Shukla <seemanshu.shukla11@gmail.com>
2024-06-11 07:18:02 +0530

    adding constants related to project secrets

---
+++ sensor/constant/env_variable.py
@@ -0,0 +1,7 @@
+##Please note that here we are just defining the env variable. That is "MONGO_DB_URL" is a env variable that
+##will be present in the .env file and we are here just referring it using MONGODB_URL_KEY variable
+
+MONGODB_URL_KEY = "MONGO_DB_URL"
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "ap-south-1"
